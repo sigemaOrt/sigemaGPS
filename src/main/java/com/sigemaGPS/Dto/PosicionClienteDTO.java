1ad92cdc4c2bbfa4b0fadd6d3202db1e6e69ccf3
joacoseo1 <joacoseo1@gmail.com>
2025-08-22 17:46:58 -0300

    werwer

--- src/main/java/com/sigemaGPS/Dto/PosicionClienteDTO.java
+++ src/main/java/com/sigemaGPS/Dto/PosicionClienteDTO.java
@@ -1,28 +1,13 @@
 package com.sigemaGPS.Dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
-
+@Getter
+@Setter
 public class PosicionClienteDTO {
-    private double latitud;
-    private double longitud;
-
-    public List<String> getEmails() {
-        return emails;
-    }
-    public void setEmails(List<String> emails) {
-        this.emails = emails;
-    }
+    private Double latitud;
+    private Double longitud;
     private List<String> emails;
-    public double getLatitud() {
-        return latitud;
-    }
-    public void setLatitud(double latitud) {
-        this.latitud = latitud;
-    }
-    public double getLongitud() {
-        return longitud;
-    }
-    public void setLongitud(double longitud) {
-        this.longitud = longitud;
-    }
 }
